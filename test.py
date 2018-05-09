2450cc969f532a05ffe909f4b2970f446b36f0b6
Lia Cha <liab_t@hotmail.com>
2018-05-09 13:47:55 -0700

    add while.md test file

--- test.py
+++ test.py
@@ -1,23 +1,6 @@
-import discord
-from discord.ext import commands
-from discord.ext.commands import Bot
-import asyncio
+import requests
 
-description = 'A bot that defines basic Python terminology'
-bot = commands.Bot(command_prefix='?', description=description)
-
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-
-@bot.command
-async def whatis(*args):
-    if args == 'for':
-        
-
-
-
-bot.run('NDQzNDIzNDY4NjI1NzIzNDEy.DdTl4Q.siQow6uu8jHZRkXdPoFOntjWF6o')
+x = 10
+while x > 5:
+  print(x)
+  x -= 1
---
+++ while.md
@@ -0,0 +1,15 @@
+While:
+
+Executes a block of code as long as the condition is true.
+Example:
+
+x = 10
+while x > 5:
+  print(x)
+  x -= 1
+  
+>>> 10
+>>> 9
+>>> 8
+>>> 7
+>>> 6
